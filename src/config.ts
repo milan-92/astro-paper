072498e6efa57f92e2f6419d60e8ed68f6e203c8
Milan <milan@thenewsavages.com>
2025-01-10 15:54:31 +0700

    changed title of blog

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const SITE: Site = {
   author: "Milan Milutinović",
   profile: "https://milanmilutinovic.com/",
   desc: "Building in Public.",
-  title: "Building Conference King in Public",
+  title: "Conference King",
   ogImage: "milan.png",
   lightAndDarkMode: true,
   postPerIndex: 4,
